db5fda2d7e679abf7242d647c334ad8a586820f0
Sakethhhhh <132201711+Sakethhhhh@users.noreply.github.com>
2024-06-26 19:26:54 +0530

    Update lazysegtree.cpp

--- lazysegtree.cpp
+++ lazysegtree.cpp
@@ -1,95 +1,64 @@
-int log2_floor(unsigned long long i) {
-    return i ? __builtin_clzll(1) - __builtin_clzll(i) : -1;
-}
-template<class T>
-class LazySegTree{
-	private:
-		const T id = 0;
-		vector<T> segtree;
-		vector<T> lazy;
-	public:
-		int len;
-		LazySegTree(int l){
-			int n = 1 << log2_floor(l);
-			if(l != n){
-				n = 1 << (log2_floor(l)+1);
-			}
-			len = n;
-			
-			segtree.resize(len*2, id);
-			lazy.resize(len*2, id);
-		}
+struct SegTree {
+    int sz;
+    vector<int64> t, lazy;
+    int64 id, lazy_id;
+    void init(int n, int64 id1, int id2) {
+        sz = n;
+        id = id1;
+        lazy_id = id2;
+        t.assign(4 * n, id);
+        lazy.assign(4 * n, lazy_id);
+    }
+    void _build(int v, vector<int64>& a, int tl, int tr) {
+        if (tl == tr) {
+            t[v] = a[tl];
+        } else {
+            int tm = (tl + tr) >> 1;
+            _build(2 * v, a, tl, tm);
+            _build(2 * v + 1, a, tm + 1, tr);
+            t[v] = max(t[2 * v], t[2 * v + 1]);
+        }
+    }
+    void build(vector<int64>& a) {
+        _build(1, a, 0, sz - 1);
+    }
+    void push(int v) {
+        t[v * 2] += lazy[v];
+        t[v * 2 + 1] += lazy[v];
+        lazy[v * 2] += lazy[v];
+        lazy[v * 2 + 1] += lazy[v];
+        lazy[v] = lazy_id;
+    }
+    void _modify(int v, int tl, int tr, int l, int r, int val) {
+        if (l > r) return;
+        if (tl == l && tr == r) {
+            t[v] += val;
+            lazy[v] += val;
+        }
+        else {
+            push(v);
+            int tm = (tl + tr) >> 1;
+            _modify(2 * v, tl, tm, l, min(tm, r), val);
+            _modify(2 * v + 1, tm + 1, tr, max(tm + 1, l), r, val);
+            t[v] = max(t[2 * v], t[2 * v + 1]);
+        }
+    }
+    void modify(int l, int r, int val) {
+        _modify(1, 0, sz - 1, l, r, val);
+    }
 
-		void update(int ind, T val){
-			ind+= len;
-			segtree[ind] = val;
-			for(; ind > 1; ind /= 2){
-				segtree[ind/2] = segtree[ind] + segtree[ind^1];
-			}
-		}
-		void pull(int node){
-			int ind = node;
-			for(; ind > 1; ind /= 2){
-				segtree[ind/2]=segtree[ind] + segtree[ind^1];
-			}
-		}
-		void push(int v){
-			lazy[2*v] += lazy[v];
-			lazy[2*v+1] += lazy[v];
-			lazy[v] = id;
-		}
-		void rupdate(int node, int nL,int nR, int l, int r, T val){
-			if(l > r) return;
-			if(nL == nR){
-				lazy[node]+= val;
-				segtree[node] += (r - l + 1) * lazy[node];
-				lazy[node] = id;
-				pull(node);
-			}
-			else if(l==nL && r==nR){
-				lazy[node]+=val;
-				segtree[node] += (r - l + 1) * lazy[node];
-				push(node);
-				pull(node);
-			}
-			else{
-				push(node);
-				int m = (nL+nR)/2;
-				rupdate(2*node,nL,m,l,min(r,m),val);
-				rupdate(2*node+1,m+1,nR,max(l,m+1),r,val);
-			}
-		}
-		void display(){
-			for(int i=0;i<len*2;i++){
-				cout<<lazy[i]<<" ";
-			}
-			cout<<"\n";
-		}
-		T _query(int node, int nL, int nR, int l, int r){
-			if(l > r) return id;
-			else if(nL==nR){
-				segtree[node]+=(r-l+1)*lazy[node];
-				lazy[node]=id;
-				pull(node);
-				return segtree[node];
-			}
-			else if(l==nL && r==nR){
-				segtree[node] += (r-l+1)*lazy[node];
-				push(node);
-				pull(node);
-				return segtree[node];
-			}
-			else{
-				push(node);
-				int m = (nL+nR)/2;
-				return (_query(node*2,nL,m,l,min(r,m))+_query(node*2+1,m+1,nR,max(l,m+1),r));
-			}
-		}
-		T query(int l, int r){
-			return _query(1,0,len-1,l,r);
-		}
-		void range_update(int l, int r, T val){
-			rupdate(1, 0, len-1,l,r,val);
-		}
-		
+    int64 _query(int v, int tl, int tr, int l, int r) {
+        if (l > r) return id;
+        if (tl == l && tr == r) {
+            return t[v];
+        }
+        else {
+            push(v);
+            int tm = (tl + tr) >> 1;
+            return max(_query(2 * v, tl, tm, l, min(tm, r)), _query(2 * v + 1, tm + 1, tr, max(tm + 1, l), r));
+        }
+    }
+    int64 query(int l, int r) {
+        return _query(1, 0, sz - 1, l, r);
+    }
 };
